ddecdb541e2a56118a8d8c71fdcbb4484e811cb1
Victor Poulsen <vmp@pop-os.localdomain>
2023-07-02 16:36:20 -0400

    new plots for n=20 case

--- analysis/plot_param_network.py
+++ analysis/plot_param_network.py
@@ -1,6 +1,5 @@
 import numpy as np 
 import pandas as pd 
-import itertools 
 import networkx as nx 
 import matplotlib.pyplot as plt 
 import os 
@@ -8,7 +7,7 @@ import seaborn as sns
 from fun import *
 
 # load relevant data 
-n_nodes, n_nan, region_type, n_rows, n_entries=38, 5, 'World_Region', 2692, 214
+n_nodes, n_nan, region_type, n_rows, n_entries=20, 10, 'World_Region', 2770, 474
 region_map={'World_Region': 'wr', 
             'NGA': 'nga'}
 region_out=region_map[region_type]
@@ -21,9 +20,9 @@ questions = pd.read_csv(f'../data/analysis/questions_with_labels_n{n_nodes}_nan{
 param = np.loadtxt(f'../data/mdl_output/{filename}.txt_lam-0.5_PNORM1_params.dat')
 
 # plot parameters
-threshold=0.95
-node_scale=500
-edge_scale=40
+threshold=0.85
+node_scale=1000
+edge_scale=10
 normalize_nodes=True
 
 # get edgelist and node dictionary (see fun.py)
@@ -33,7 +32,9 @@ d_edgelist, dct_nodes = node_edge_lst(n_nodes, param)
 G, labeldict = create_graph(d_edgelist, dct_nodes)
 
 # remove edges below threshold (see fun.py)
-G = remove_edges(G, threshold)
+G = remove_edges(G, 
+                 d_edgelist,
+                 threshold)
 
 # label the questions (see fun.py) 
 labeldict = label_questions(G, questions, 'Question index', 'Question Short')
@@ -56,8 +57,8 @@ edge_size = [abs(x) for x in edge_color]
 # plot network (see fun.py)
 plot_network(G, 
              labeldict,
-             vmin_nodes=(node_vmin, node_vmax),
-             vmax_nodes=(node_vmin, node_vmax),
+             scale_nodes=(node_vmin, node_vmax),
+             scale_edges=(edge_vmin, edge_vmax),
              node_size=node_size,
              node_color=node_color,
              edge_size=edge_size,
